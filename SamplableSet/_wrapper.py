4da93be065182721aea3bbdb4defa242296cc3d9
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2018-10-20 16:37:01 -0400

    new constructor

--- SamplableSet/_wrapper.py
+++ SamplableSet/_wrapper.py
@@ -29,40 +29,57 @@ template_classes = {
 
 class SamplableSet:
     """
-    This class implements a set which is samplable in O(1) time according to the weight distribution of the elements.
+    This class implements a set which is samplable in O(1) time according to the weight distribution of the elements. Elements can either be an integer or a tuple of 3 integers.
 
-    The SamplableSet can be instanciated with parameters or from a pre-existing one.
+    The SamplableSet can be instanciated empty or from an iterable of 2 iterables of elements and weights respectively.
 
     This class is a wrapper around a C++ implementation.
     """
-    def __init__(self, *, samplable_set=None, min_weight=None, max_weight=None, seed=None, cpp_type='int'):
+    def __init__(self, min_weight, max_weight, elements_weights=None, seed=None, cpp_type='int'):
         """
         Creates a new SamplableSet instance.
 
         Args:
-            samplable_set (SamplableSet instance, optional): An instance of SamplableSet to be copied. The new object will have the same elements with the same weights. If not None, all other parameters will be ignored.
-            min_weight (float, optional): Minimum weight a given element can have. This is needed for a good repartition of the elements inside the internal tree structure. Necessary if 'samplable_set' is None.
-            max_weight (float, optional): Minimum weight a given element can have. This is needed for a good repartition of the elements inside the internal tree structure. Necessary if 'samplable_set' is None.
+            min_weight (float): Minimum weight a given element can have. This is needed for a good repartition of the elements inside the internal tree structure.
+            max_weight (float): Maximum weight a given element can have. This is needed for a good repartition of the elements inside the internal tree structure.
+            elements_weights (iterable of 2 iterables or dict, optional): If an iterable, should be a pair of 2 iterables of same lengths with which the set will be instanciated. The first iterable should be the elements and the second should be the respective weights. If a dict, keys should be the elements and values should be the weights. If not specified, the set will be empty.
             seed (float, optional): Seed used to sample elements from the set.
-            cpp_type (str, optional, either 'int' or 'edge'): Type used in the C++ implementation.
+            cpp_type (str, optional, either 'int' or 'edge'): Type used in the C++ implementation. 'edge' should be a tuple of 3 integers. If 'elements_weights' is specified, the type will be infered from it.
         """
-        if samplable_set is not None:
-            self._samplable_set = samplable_set.copy()
-
-        elif isinstance(min_weight, (int, float)) and isinstance(max_weight, (int, float)):
-            if seed is None:
-                self._samplable_set = template_classes[cpp_type](min_weight, max_weight)
+        # Unpacking
+        if elements_weights:
+            if isinstance(elements_weights, dict):
+                elements = elements_weights.keys()
+                weights = elements_weights.values()
             else:
-                self._samplable_set = template_classes[cpp_type](min_weight, max_weight, seed)
+                elements, weights = elements_weights
 
-            for func_name in ['size', 'total_weight', 'count', 'insert', 'set_weight', 'erase']:
-                setattr(self, func_name, getattr(self._samplable_set, func_name))
+            # Infering cpp_type
+            first_element = next(iter(elements))
+            first_weight = next(iter(weights))
+            cpp_type = 'int' if first_element is int else 'edge'
 
-            self.max_weight = max_weight
-            self.min_weight = min_weight
+            # Validation
+            if len(elements) != len(weights):
+                raise ValueError("'elements' and 'weights' should have the same length.")
 
+        # Instanciate the set
+        if seed is None:
+            self._samplable_set = template_classes[cpp_type](min_weight, max_weight)
         else:
-            raise ValueError('Minimum and maximum weights are needed to construct the set.')
+            self._samplable_set = template_classes[cpp_type](min_weight, max_weight, seed)
+
+        for func_name in ['size', 'total_weight', 'count', 'insert', 'set_weight', 'erase']:
+            setattr(self, func_name, getattr(self._samplable_set, func_name))
+
+        self.max_weight = max_weight
+        self.min_weight = min_weight
+
+        # Initialize the set
+        if elements_weights:
+            self[first_element] = first_weight
+            for element, weight in zip(elements, weights):
+                self[element] = weight
 
     def __contains__(self, element):
         return True if self.count(element) else False
