5c13a350946c5b139d1c1989e3ce4a28523e3998
Guillaume St-Onge <guillaume.st-onge.4@ulaval.ca>
2019-09-08 00:31:23 -0400

    Python generator tested and working

--- test/test_iterator.py
+++ test/test_iterator.py
@@ -8,7 +8,7 @@ Author: Guillaume St-Onge <guillaume.st-onge.4@ulaval.ca>
 
 from SamplableSet import SamplableSet
 
-class TestBasicMethod:
+class TestCppMethod:
     def test_init_iterator_1(self):
         #elements in same group
         elements = {1:10, 2:10}
@@ -39,5 +39,19 @@ class TestBasicMethod:
         s.next()
         assert s.get_at_iterator() == (2,50.)
 
+    def test_next_get_None(self):
+        elements = {1:10, 2:50}
+        s = SamplableSet(1,100,elements)
+        s.init_iterator()
+        s.next()
+        s.next()
+        assert s.get_at_iterator() is None
 
+
+def test_python_generator():
+    elements = range(1,10)
+    weights = range(1,10)
+    s = SamplableSet(1,10,zip(elements,weights))
+    expected = zip(elements,weights)
+    assert list(expected) == list(s.__iter__())
 
